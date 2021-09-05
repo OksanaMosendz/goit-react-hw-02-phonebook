1edf5d8d16ecfdc612db85e558c5feb6e07a2588
OksanaMosendz <bagheera03@gmail.com>
2021-09-05 22:48:26 +0300

    add contactform styles

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,58 +1,61 @@
-import React from "react";
-
+import React from 'react';
+import { Form, FormInput } from './ContactForm.styled';
 
 export class ContactForm extends React.Component {
   state = {
-   name: '',
-   number: '',
- }
- 
-  HandleNameInput=(e)=>{
+    name: '',
+    number: '',
+  };
+
+  HandleNameInput = e => {
     this.setState({ name: e.target.value });
   };
 
-  HandleNumberInput=(e)=>{
+  HandleNumberInput = e => {
     this.setState({ number: e.target.value });
   };
-  
-  FormSubmit = (e) => {
-    const { name, number} = this.state;
+
+  FormSubmit = e => {
+    const { name, number } = this.state;
     e.preventDefault();
     this.props.onSubmit(name, number);
     this.setState({ name: '', number: '' });
-    }
+  };
 
- 
   render() {
-  const { name, number } = this.state;
+    const { name, number } = this.state;
     return (
+      <Form>
+        <form onSubmit={this.FormSubmit}>
+           <FormInput><label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.HandleNameInput}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </label>< /FormInput>
 
-      <form onSubmit={this.FormSubmit}>
-        <label>Name
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.HandleNameInput}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required />
-        </label>
+          <FormInput> <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={number}
+              onChange={this.HandleNumberInput}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+              required
+            />
+          </label> </FormInput>
 
-        <label >Number
-          <input
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.HandleNumberInput}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
-          />
-        </label>
-
-        <button type="submit">Add contact</button>
-      </form>
-    )
+          <button type="submit">Add contact</button>
+        </form>
+      </Form>
+    );
   }
 }
---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  border: 1 px black solid;
+`;
+
+export const Label = styled.label`
+margin - bottom: 10 px;
+`;
